849ad20e39b148c635c5b426957515e034466f74
Hooi Tan <Hooi.Tan@insight.com>
2019-09-05 17:48:58 +0800

    #7429 Configured AppCenter analytics and crashes

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { AppComponent } from "./app.component";
 import { BarcodeScanner } from "@ionic-native/barcode-scanner/ngx";
 import { FormsModule } from "@angular/forms";
 import { Camera } from '@ionic-native/camera/ngx';
+import { AppCenterAnalytics } from '@ionic-native/app-center-analytics/ngx';
+import { AppCenterCrashes } from '@ionic-native/app-center-crashes/ngx';
 
 import { AddItemPageModule } from "./modals/add-item/add-item.module";
 import { RecipesPageModule } from "./recipes/recipes.module"
@@ -33,7 +35,19 @@ import { RecipesPageModule } from "./recipes/recipes.module"
     BarcodeScanner,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     Camera,
+    AppCenterAnalytics,
+    AppCenterCrashes,
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(
+    private appCenterCrashes: AppCenterCrashes,
+  ){
+    this.appCenterCrashes.setEnabled(true).then(() => {    
+      this.appCenterCrashes.lastSessionCrashReport().then(report => {
+          console.log('Crash report', report);
+      });
+   });
+  }  
+}
