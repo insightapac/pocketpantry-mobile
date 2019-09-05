42e82ff4b0a1533d31e2a49fb3c9a9e1ff1839f6
Max Leong <max.leong@insight.com>
2019-09-06 00:12:08 +0800

    Searchable add-item.page with fake data

--- src/client/src/app/modals/add-item/add-item.page.ts
+++ src/client/src/app/modals/add-item/add-item.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from "@ionic/angular";
+import { DataService } from "../../services/data.service"
 
 @Component({
   selector: 'app-add-item',
@@ -7,11 +8,28 @@ import { ModalController } from "@ionic/angular";
   styleUrls: ['./add-item.page.scss'],
 })
 export class AddItemPage implements OnInit {
+  public searchTerm: string = "";
+  public items: any;
 
-  constructor(private modalController: ModalController,) { }
+  constructor(private modalController: ModalController, private dataService: DataService) { }
+
+  doRefresh(event) {
+    console.log("Begin async operation");
+
+    setTimeout(() => {
+      console.log("Async operation has ended");
+      event.target.complete();
+    }, 1000);
+  }
 
   ngOnInit() {
+    this.setFilteredItems();
   }
+
+  setFilteredItems() {
+    this.items = this.dataService.filterItems(this.searchTerm);
+  }
+  
   dismiss() {
     this.modalController.dismiss({
       dismissed: true
